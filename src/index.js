62fc9fd77cbe758f298a4eb0b988548a2547b32c
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-01-05 17:19:48 +0300

    Connected the forms from the HTML to the Javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const taskInput = event.target['new-task-description'];
+    buildToDo(taskInput);
+
+    event.target.reset();
+  });
