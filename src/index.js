e1c27e7cc197189563b80ce1d89c091f581fbb8e
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-01-05 19:07:39 +0300

    Added necessary changes after countless tests

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
 
-  form.addEventListener('submit', (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const task = taskInput.value.trim();
 
-    const taskInput = event.target['new-task-description'];
-    buildToDo(taskInput);
-
-    event.target.reset();
+    if (task !== '') {
+      buildToDo(task, taskList);
+      form.reset();
+    }
   });
 });
 
-function buildToDo(task) {
+function buildToDo(task, taskList) {
   const li = document.createElement('li');
-  li.textContent = task
-
-  const taskList = document.getElementById('tasks');
+  li.textContent = task;
   taskList.appendChild(li);
 }
